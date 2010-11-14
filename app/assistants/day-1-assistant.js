db33e58b6fef3ed47f2c116ad4109e06795f2984
Robert Kowalski <rok@lavabit.com>
2010-11-14 13:55:54 +0100

    license headers + buttons

--- app/assistants/day-1-assistant.js
+++ app/assistants/day-1-assistant.js
@@ -1,14 +1,66 @@
+/*
+Copyright (c) 2010, R. Kowalski
+All rights reserved.
+
+
+Redistribution and use in source and binary forms, 
+with or without modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, 
+  this list of conditions and the following disclaimer.
+  
+* Redistributions in binary form must reproduce the above copyright notice, 
+  this list of conditions and the following disclaimer in the documentation and/or other materials provided 
+  with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING 
+IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
 function Day1Assistant() {
-	/* this is the creator function for your scene assistant object. It will be passed all the 
-	   additional parameters (after the scene name) that were passed to pushScene. The reference
-	   to the scene controller (this.controller) has not be established yet, so any initialization
-	   that needs the scene controller should be done in the setup function below. */
+	
 }
 
 Day1Assistant.prototype.setup = function() {
 
-  
-
+     this.controller.setupWidget("saal1",
+         this.attributes = {
+             },
+         this.model = {
+             label : "Saal 1",
+             disabled: false
+         }
+     );
+     this.controller.setupWidget("saal2",
+         this.attributes = {
+             },
+         this.model = {
+             label : "Saal 2",
+             disabled: false
+         }
+     );
+     this.controller.setupWidget("saal3",
+         this.attributes = {
+             },
+         this.model = {
+             label : "Saal 3",
+             disabled: false
+         }
+     );
+/*     
+    this.openSaal = this.openSaal.bindAsEventListener(this); //PRE-CACHE//	
+    Mojo.Event.listen(this.controller.get('saal1'), Mojo.Event.tap, this.openSaal('1'));
+	Mojo.Event.listen(this.controller.get('saal2'), Mojo.Event.tap, this.openSaal('2'));
+	Mojo.Event.listen(this.controller.get('saal3'), Mojo.Event.tap, this.openSaal('3'));
+*/	
 }
 
 Day1Assistant.prototype.activate = function(event) {
