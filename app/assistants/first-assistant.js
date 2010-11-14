a884a89a73b15ff33718fcbff146a06fc18eb189
Robert Kowalski <rok@lavabit.com>
2010-11-14 14:59:43 +0100

    remaining scenes & framework_config, bugfix

--- app/assistants/first-assistant.js
+++ app/assistants/first-assistant.js
@@ -43,8 +43,9 @@ FirstAssistant.prototype.setup = function() {
             items: Congress.menu
         }
     );
+   
     this.openMenuItem = this.openMenuItem.bindAsEventListener(this); //PRE-CACHE//
-    this.controller.listen("MenuWidget", Mojo.Event.listTap,this.openMenuItem);
+
     
     var time =  Mojo.Format.formatDate(new Date(), {
 		time: "medium",
@@ -55,21 +56,22 @@ FirstAssistant.prototype.setup = function() {
 }
 
 FirstAssistant.prototype.activate = function(event) {
-    this.controller.modelChanged(this.menuModel); 
+	this.controller.listen("MenuWidget", Mojo.Event.listTap,this.openMenuItem);
 }
 
 
 FirstAssistant.prototype.deactivate = function(event) {
-    this.controller.stopListening("MenuWidget", Mojo.Event.listTap,this.openMenuItem);
+	this.controller.stopListening("MenuWidget", Mojo.Event.listTap,this.openMenuItem);
+
 }
 
 FirstAssistant.prototype.cleanup = function(event) {
-
+   
+   
 }
 
 FirstAssistant.prototype.openMenuItem = function(event) {
     
-//    Mojo.Log.error(this.menuModel.items[event.index].scene);
     Mojo.Controller.stageController.pushScene(this.menuModel.items[event.index].scene);
 
 };
